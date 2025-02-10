4662595919e709ac02c1d29b61d6b48122a9f282
rakul2005 <rakul2005@icloud.com>
2025-02-10 16:16:39 +0100

    Update project1.py

--- project1.py
+++ project1.py
@@ -1,1 +1,63 @@
-print("hello world!")
+#Day 1: Python Basics & Data Types
+
+# data types, and basic operations
+# 1️⃣ Topics to Cover
+
+# ✅ Printing & Variables
+# ✅ Data Types (int, float, string, bool)
+# ✅ Basic Operators (+, -, *, /, //, %, **)
+# ✅ Type Conversion (int(), float(), str(), bool())
+
+#printing
+
+print("Hello World!")
+
+
+# Variables
+
+name = "John"
+age = 25
+height = 5.7
+
+print(f"Hello my name is {name}, I am {age} years old and I am {height}ft tall!")
+
+# data types (int, float, string, bool)
+
+# integer and floats
+a = 3.5
+b = 7
+
+sum_value = a + b
+diff_value = a - b
+mult_value = a * b
+floor_div = a // b
+modulus = a % b
+power = a**b
+
+print(sum_value, diff_value, mult_value, floor_div, modulus, power)
+
+# Type Conversion
+
+x = "100"
+x_int = int(x)
+
+y = 2.5
+y_str = str(y) 
+
+print(int(x), str(y))
+
+#boolean logic
+
+is_sunny = True
+is_raining = False
+
+print(is_sunny and not is_raining)
+print(is_sunny or is_raining)
+print(not is_sunny and not is_raining)
+
+
+
+
+
+
+
