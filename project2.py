f47ffd2fc274c4e91c5675e9dd6f4d41e6297726
r.rakulcodes <126799162+rakul2005@users.noreply.github.com>
2025-02-11 16:02:59 +0100

    Add files via upload

---
+++ project2.py
@@ -0,0 +1,21 @@
+#📅 Day 2: Python Data Structures (Lists, #Tuples, Sets, and Dictionaries)
+
+#🔹 Topics for Today
+#Today, we’ll dive into Python’s data #structures—fundamental for handling data in #Machine Learning & AI.
+
+#✅ Lists (Dynamic arrays)
+#✅ Tuples (Immutable sequences)
+#✅ Sets (Unique unordered collections)
+#✅ Dictionaries (Key-value pairs)
+
+#lists - ordered & mutable 
+
+#creating a list
+fruits = ["apple","banana", "cherry"]
+print(fruits)
+
+#modifying elements
+fruits[1] = "blueberry"
+print(fruits)
+
+
